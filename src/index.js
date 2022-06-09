a13c8a668661c8159a081f8c9876b9fffee56f3d
Maria Laura Altamirano <laltamirano@redsos.com.ar>
2022-06-09 12:51:03 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
