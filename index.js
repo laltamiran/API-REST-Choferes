0fcbb2595e2385f7edbfad87efea1917b6150f2f
Maria Laura Altamirano <laltamirano@redsos.com.ar>
2022-06-28 13:55:38 -0300

    archivo principal js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const app = express();
+const morgan = require('morgan');
+const fetch = require('node-fetch');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+app.use('/api/movies', require('./routes/movies'));
+app.use('/api/users', require('./routes/users'));
+
+//server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
